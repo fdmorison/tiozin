9cc5869c65351c09ebe69bebb192f20eb8f5aec3
Fabrício D'Morison <fabricio.dmorison@gmail.com>
2025-12-06 23:12:58 -0300

    Feat: Skeletons for Job, JobBuilder, Resource, RegistryFactory, Logs and Context (#4)
    * feat: add core skeletons
    
    * docs: docstrings

---
+++ tio/model/job.py
@@ -0,0 +1,29 @@
+import time
+
+from .abc.resource import Resource
+from .context import Context
+
+
+class Job(Resource):
+    """
+    Represents a complete ETL process within a TioApp.
+
+    A Job is composed of one or more Inputs, one or more Transforms, one or more Outputs,
+    and a Runner, forming the major execution step inside a TioApp. It encapsulates all the
+    logic needed to extract, transform, and load data according to the job manifest.
+
+    Jobs are built from manifests, whose most basic form is YAML files stored on the
+    filesystem, in a bucket, or in a database.
+
+    Jobs are executed within the TioApp, which is responsible for managing registries,
+    configuration, and other runtime information.
+    """
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    def run(self, context: Context) -> None:
+        self.logger.info(f"The job `{self}` is starting.")
+        time.sleep(3)
+        self.logger.info(f"The job `{self}` has finished with success.")
+        return
