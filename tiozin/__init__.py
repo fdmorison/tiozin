f43701b39ab5d6568b4899eb92583a5ce9e5dd2e
Fabrício D'Morison <fabricio.dmorison@gmail.com>
2025-12-20 20:57:47 -0400

    feat: easy imports

--- tiozin/__init__.py
+++ tiozin/__init__.py
@@ -1,6 +1,13 @@
 # isort: skip_file
 # flake8: noqa
 
-from .model.job import Job as Job
+from .model.service import Service as Service
 from .model.resource import Resource as Resource
+from .model.plugable import Plugable as Plugable
+from .model.runner import Runner as Runner
+from .model.transform import Transform as Transform
+from .model.input import Input as Input
+from .model.output import Output as Output
+
+from .model.job import Job as Job
 from .model.context import Context as Context
