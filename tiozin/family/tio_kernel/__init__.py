cbe3faef551fe90feec3f107df5def87bb559456
Fabrício D'Morison <fabricio.dmorison@gmail.com>
2025-12-28 02:06:13 -0400

    Refactor: Separate plugin discovery into PluginScanner and remove unused code (#30)
    * feat-plugin-scanner

--- tiozin/family/tio_kernel/__init__.py
+++ tiozin/family/tio_kernel/__init__.py
@@ -5,3 +5,11 @@ from .inputs.noop_input import NoOpInput as NoOpInput
 from .outputs.noop_output import NoOpOutput as NoOpOutput
 from .transforms.noop_transform import NoOpTransform as NoOpTransform
 from .runners.noop_runner import NoOpRunner as NoOpRunner
+
+from .registries import FileJobRegistry as FileJobRegistry
+from .registries import NoOpMetricRegistry as NoOpMetricRegistry
+from .registries import NoOpLineageRegistry as NoOpLineageRegistry
+from .registries import NoOpSchemaRegistry as NoOpSchemaRegistry
+from .registries import NoOpSecretRegistry as NoOpSecretRegistry
+from .registries import NoOpSettingRegistry as NoOpSettingRegistry
+from .registries import NoOpTransactionRegistry as NoOpTransactionRegistry
