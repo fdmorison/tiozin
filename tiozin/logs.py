f9c3608b3212349bd0c7fa0e07bba0e19282fa63
Fabrício D'Morison <fabricio.dmorison@gmail.com>
2026-01-03 21:11:58 -0400

    feat: added TiozinLogger

--- tiozin/logs.py
+++ tiozin/logs.py
@@ -1,31 +1,16 @@
 import logging
-from typing import Any
 
 import structlog
 
 from . import config
-
-BLUE = "\033[34m"
-CYAN = "\033[36m"
-RESET = "\033[0m"
-BOLD = "\033[1m"
+from .api.logger import TiozinLogger
 
 CONSOLE_RENDERER = structlog.dev.ConsoleRenderer(colors=True)
 JSON_RENDERER = structlog.processors.JSONRenderer(ensure_ascii=config.log_json_ensure_ascii)
-PADDING_MAX_LENGTH = 15
 
 
-def get_logger(name: str) -> logging.Logger:
-    return structlog.getLogger(logname=name)
-
-
-def custom_tiozin_renderer(_, __, event_dict: dict[str, Any]) -> dict[str, Any]:
-    if config.log_json:
-        return event_dict
-    logname: str = event_dict.pop("logname", "root")
-    logname = f"{BLUE}{logname.ljust(PADDING_MAX_LENGTH)}{RESET}"
-    event_dict["event"] = f"[{logname}] {event_dict['event']}"
-    return event_dict
+def get_logger(name: str) -> TiozinLogger:
+    return TiozinLogger(name)
 
 
 def setup() -> None:
@@ -42,10 +27,9 @@ def setup() -> None:
             structlog.processors.StackInfoRenderer(),
             structlog.processors.TimeStamper(fmt=config.log_date_format, utc=True),
             structlog.dev.set_exc_info,
-            custom_tiozin_renderer,
             JSON_RENDERER if config.log_json else CONSOLE_RENDERER,
         ],
         wrapper_class=structlog.make_filtering_bound_logger(config.log_level),
         logger_factory=structlog.PrintLoggerFactory(),
-        cache_logger_on_first_use=False,
+        cache_logger_on_first_use=True,
     )
