0d691605a507ddce8b20f636c068a79d7b766b25
Fabrício D'Morison <fabricio.dmorison@gmail.com>
2026-01-28 01:26:47 -0400

    feat(tio_spark): declare Spark provider public API

--- tiozin/family/tio_spark/__init__.py
+++ tiozin/family/tio_spark/__init__.py
@@ -1,6 +1,45 @@
-# isort: skip_file
-# flake8: noqa
+"""
+tio_spark public API.
 
-from .inputs.file_input import SparkFileInput as SparkFileInput
-from .outputs.file_output import SparkFileOutput as SparkFileOutput
-from .runners.spark_runner import SparkRunner as SparkRunner
+This module defines the public, stable interface of the Spark provider
+used by Tiozin pipelines.
+
+Only the symbols exported here are considered part of the supported API.
+All other modules and classes inside this package are internal implementation
+details and may change without notice.
+"""
+
+# Public base abstractions
+from .bases import (
+    SparkCoTransform,
+    SparkInput,
+    SparkOutput,
+    SparkTransform,
+)
+
+# Inputs
+from .inputs.file_input import SparkFileInput
+
+# Outputs
+from .outputs.file_output import SparkFileOutput
+from .runners.spark_iceberg_runner import SparkIcebergRunner
+
+# Runners
+from .runners.spark_runner import SparkRunner
+
+# Transforms
+from .transforms.sql_transform import SparkSqlTransform
+from .transforms.word_count_transform import SparkWordCountTransform
+
+__all__ = [
+    "SparkInput",
+    "SparkTransform",
+    "SparkCoTransform",
+    "SparkOutput",
+    "SparkRunner",
+    "SparkIcebergRunner",
+    "SparkFileInput",
+    "SparkFileOutput",
+    "SparkSqlTransform",
+    "SparkWordCountTransform",
+]
