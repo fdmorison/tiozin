a3eb2b80083fc4e5c014940dee6a520f71178205
Fabrício D'Morison <fabricio.dmorison@gmail.com>
2026-02-10 00:11:23 -0400

    test: add context fixtures

---
+++ tests/conftest.py
@@ -0,0 +1,62 @@
+import pytest
+
+from tiozin import Context
+
+
+@pytest.fixture()
+def fake_taxonomy() -> dict:
+    return dict(
+        org="acme",
+        region="latam",
+        domain="ecommerce",
+        layer="raw",
+        product="sales",
+        model="orders",
+    )
+
+
+@pytest.fixture()
+def job_context(fake_taxonomy: dict) -> Context:
+    return Context(
+        name="test_job",
+        kind="LinearJob",
+        plugin_kind="job",
+        **fake_taxonomy,
+        options={},
+    )
+
+
+@pytest.fixture()
+def input_context(fake_taxonomy: dict, job_context: Context) -> Context:
+    return Context(
+        name="test_input",
+        kind="TestInput",
+        plugin_kind="Input",
+        parent=job_context,
+        **fake_taxonomy,
+        options={},
+    )
+
+
+@pytest.fixture()
+def transform_context(fake_taxonomy: dict, job_context: Context) -> Context:
+    return Context(
+        name="test_transform",
+        kind="TestTransform",
+        plugin_kind="Transform",
+        parent=job_context,
+        **fake_taxonomy,
+        options={},
+    )
+
+
+@pytest.fixture()
+def output_context(fake_taxonomy: dict, job_context: Context) -> Context:
+    return Context(
+        name="test_output",
+        kind="TestOutput",
+        plugin_kind="Output",
+        parent=job_context,
+        **fake_taxonomy,
+        options={},
+    )
