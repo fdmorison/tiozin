4ff4b8db625c1360f3c4b9fdd108ed4b342d9c23
Fabrício D'Morison <fabricio.dmorison@gmail.com>
2026-02-10 01:13:32 -0300

    fix(core): Template rendering in plugin setup and teardown (#115)

---
+++ tests/stubs/__init__.py
@@ -0,0 +1,13 @@
+from tests.stubs.input import InputStub
+from tests.stubs.job import JobStub
+from tests.stubs.output import OutputStub
+from tests.stubs.runner import RunnerStub
+from tests.stubs.transform import StubTransform
+
+__all__ = [
+    "InputStub",
+    "JobStub",
+    "OutputStub",
+    "RunnerStub",
+    "StubTransform",
+]
