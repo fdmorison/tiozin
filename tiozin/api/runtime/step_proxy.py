426d31abfaf340ae7c84a0a695197fdacfe0f71a
Fabrício D'Morison <fabricio.dmorison@gmail.com>
2026-03-30 18:39:13 -0400

    refactor: integrate catalog and dataset into proxies

--- tiozin/api/runtime/step_proxy.py
+++ tiozin/api/runtime/step_proxy.py
@@ -4,14 +4,15 @@ from typing import TYPE_CHECKING, Any
 
 import wrapt
 
-from tiozin.api import Context, LineageDataset
+from tiozin.api import Context
 from tiozin.exceptions import AccessViolationError, TiozinInternalError
 from tiozin.utils import utcnow
 
 from ...compose import TiozinTemplateOverlay
+from .dataset import Dataset
 
 if TYPE_CHECKING:
-    from tiozin import EtlStep, Lineage
+    from tiozin import EtlStep
 
 
 class StepProxy(wrapt.ObjectProxy):
@@ -22,59 +23,47 @@ class StepProxy(wrapt.ObjectProxy):
     context propagation, template rendering, lifecycle hooks, logging, and timing.
     """
 
-    def __init__(self, wrapped) -> None:
-        super().__init__(wrapped)
-        self._self_schema = None
-
     def setup(self, *args, **kwargs) -> None:
         raise AccessViolationError(self)
 
     def teardown(self, *args, **kwargs) -> None:
         raise AccessViolationError(self)
 
-    def read(self) -> None:
+    def read(self) -> Any:
         return self._execute()
 
-    def transform(self, *args, **kwargs) -> None:
+    def transform(self, *args, **kwargs) -> Any:
         return self._execute(*args, **kwargs)
 
-    def write(self, *args, **kwargs) -> None:
+    def write(self, *args, **kwargs) -> Any:
         return self._execute(*args, **kwargs)
-
-    def lineage_datasets(self) -> Lineage:
-        step: EtlStep = self.__wrapped__
-        context = Context.for_step(step)
-        with context, TiozinTemplateOverlay(step, context.template_vars):
-            runtime_dataset = LineageDataset(
-                namespace=context.namespace,
-                name=context.qualified_slug,
-                schema=self._self_schema,
-            )
-            static_datasets = step.lineage_datasets()
-            for dataset in static_datasets.inputs:
-                dataset.merge(runtime_dataset)
-            return static_datasets
 
     def _execute(self, *args, **kwargs) -> Any:
         from tiozin import Input, Output, Transform
 
         step: EtlStep = self.__wrapped__
         context = Context.for_step(step)
+        catalog = context.catalog
+
+        static = step.static_datasets()
 
         with context, TiozinTemplateOverlay(step, context.template_vars):
             try:
                 step.info(f"▶️  Starting to {context.tiozin_role} data")
                 step.debug(f"Temporary workdir is {context.temp_workdir}")
+
+                catalog.register(step, inputs=[*static.inputs, *args])
+
                 context.setup_at = utcnow()
                 step.setup(*args, **kwargs)
                 context.executed_at = utcnow()
+
                 match step:
                     case Input():
                         context.schema = context.registries.schema.try_get(
                             step.schema_subject,
                             step.schema_version,
                         )
-                        self._self_schema = context.schema
                         result = step.read(*args, **kwargs)
                     case Transform():
                         result = step.transform(*args, **kwargs)
@@ -82,12 +71,32 @@ class StepProxy(wrapt.ObjectProxy):
                         result = step.write(*args, **kwargs)
                     case _:
                         raise TiozinInternalError(f"Not a Tiozin: {step}")
+
             except Exception:
                 step.error(f"{context.kind} failed in {context.execution_delay:.2f}s")
                 raise
+
             else:
                 step.info(f"{context.kind} finished in {context.execution_delay:.2f}s")
+
+                # output dataset
+                dataset = Dataset.wrap(result)
+
+                # infra datasets first
+                for static_output in static.outputs:
+                    dataset.merge(static_output)
+
+                # runtime fallback
+                dataset.with_namespace(context.namespace).with_name(context.qualified_slug)
+
+                # schema fallback
+                if context.schema:
+                    dataset.with_schema(context.schema)
+
+                catalog.register(step, output=dataset)
+
                 return result
+
             finally:
                 context.teardown_at = utcnow()
                 try:
